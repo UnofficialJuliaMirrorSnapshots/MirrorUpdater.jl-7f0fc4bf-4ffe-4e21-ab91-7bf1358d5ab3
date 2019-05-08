a561e39cc4d1e2dde428c71fb94027eb7b73bbf7
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-08 01:01:16 +0000

    Snapshot of branch master taken on 2019-05-07T21:01:15.79-04:00 from "https://github.com/UnofficialJuliaMirror/MirrorUpdater.jl-7f0fc4bf-4ffe-4e21-ab91-7bf1358d5ab3"

--- config/repositories/unregistered-packages.jl
+++ config/repositories/unregistered-packages.jl
@@ -2,37 +2,16 @@ const UNREGISTERED_PACKAGES = MirrorUpdater.Types.SrcDestPair[
     MirrorUpdater.Types.SrcDestPair(
         ;
         source_url =
-            "https://github.com/UnofficialJuliaMirror/MirrorUpdater.jl",
+            "https://github.com/DilumAluthge/DelayedErrors.jl",
         destination_repo_name =
-            "MirrorUpdater.jl-7f0fc4bf-4ffe-4e21-ab91-7bf1358d5ab3",
+            "DelayedErrors.jl-b0cb2517-35d6-4513-a0ca-b0b388a189e4",
         ),
     MirrorUpdater.Types.SrcDestPair(
         ;
         source_url =
-            "https://github.com/UnofficialJuliaMirrorSnapshots/Snapshots.jl",
+            "https://github.com/JuliaMath/Libm.jl",
         destination_repo_name =
-            "Snapshots.jl-44eb87bc-f37b-45e8-9f53-3bcb453a652d",
-        ),
-    MirrorUpdater.Types.SrcDestPair(
-        ;
-        source_url =
-            "https://github.com/alan-turing-institute/MLJ.jl",
-        destination_repo_name =
-            "MLJ.jl-add582a8-e3ab-11e8-2d5e-e98b27df1bc7",
-        ),
-    MirrorUpdater.Types.SrcDestPair(
-        ;
-        source_url =
-            "https://github.com/alan-turing-institute/MLJBase.jl",
-        destination_repo_name =
-            "MLJBase.jl-a7f614a8-145f-11e9-1d2a-a57a1082229d",
-        ),
-    MirrorUpdater.Types.SrcDestPair(
-        ;
-        source_url =
-            "https://github.com/alan-turing-institute/MLJModels.jl",
-        destination_repo_name =
-            "MLJModels.jl-d491faf4-2d78-11e9-2867-c94bc002c0b7",
+            "JuliaMath-Libm.jl",
         ),
     MirrorUpdater.Types.SrcDestPair(
         ;
@@ -40,5 +19,26 @@ const UNREGISTERED_PACKAGES = MirrorUpdater.Types.SrcDestPair[
             "https://github.com/alan-turing-institute/MLJRegistry.jl",
         destination_repo_name =
             "MLJRegistry.jl-a41df0fa-2d8a-11e9-1bfb-8110be68cd3e",
+        ),
+    MirrorUpdater.Types.SrcDestPair(
+        ;
+        source_url =
+            "https://github.com/UnofficialJuliaMirror/MirrorUpdater.jl",
+        destination_repo_name =
+            "MirrorUpdater.jl-7f0fc4bf-4ffe-4e21-ab91-7bf1358d5ab3",
+        ),
+    MirrorUpdater.Types.SrcDestPair(
+        ;
+        source_url =
+            "https://github.com/UnofficialJuliaMirrorSnapshots/RemoveLFS.jl",
+        destination_repo_name =
+            "RemoveLFS.jl-a262596c-dd27-4d54-b019-3619024addbd",
+        ),
+    MirrorUpdater.Types.SrcDestPair(
+        ;
+        source_url =
+            "https://github.com/UnofficialJuliaMirrorSnapshots/Snapshots.jl",
+        destination_repo_name =
+            "Snapshots.jl-44eb87bc-f37b-45e8-9f53-3bcb453a652d",
         ),
     ]
